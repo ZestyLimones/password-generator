85519e72c4cfaa700ddc553562d443446b4173b1
Micheal Limones <Micheal.Limones@du.edu>
2021-07-12 19:41:04 -0600

    created function to randomly go through all possible options array. worked with Eric to create prompt function.

--- script.js
+++ script.js
@@ -1,74 +1,152 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//empty arrays for variables
-let allPossibleSpecialChar = ["!", "#", "$", "%", "&", "*", "+", "-", "?", "@"];
-let allPossibleUpperCase = ["A", "B" , "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let allPossibleLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let allPossibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let allPossibleOptions = [];
+//arrays for all possible characters
+var allPossibleSpecialChar = ["!", "#", "$", "%", "&", "*", "+", "-", "?", "@"];
+var allPossibleUpperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var allPossibleLowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var allPossibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-function userPrompt() {
-  var passwordLength = prompt("How long do you want your password to be?");
+//emplty array to store my character options
+var allPossibleOptions = [];
 
+//variable for my password length question
+var passwordLength;
+
+//function to determine if it is prompt, confirm, or alert
+function generatePrompts(question, type, characterSet) {
+  var answer;
+  if (type === "prompt") {
+    answer = prompt(question);
+  } else if (type === "confirm") {
+    answer = confirm(question);
+    if (answer === true) {
+      allPossibleOptions = allPossibleOptions.concat(characterSet);
+    }
+  } else {
+    answer = alert(question);
+  }
+  return answer;
+}
+
+//function to create password options
+function createPassword() {
+  //intial prompt to get password length
+  passwordLength = generatePrompts(
+    "How long do you want your password to be?",
+    "prompt"
+  );
+  //verifying password is within the length requirements
   if (passwordLength < 8) {
-    alert("password must be at least 8 characters long.")
-    userPrompt();
-  } else if (password > 128) {
-    alert("password must be no more than 128 characters.")
+    generatePrompts("password must be at least 8 characters long.");
+    createPassword();
+  } else if (passwordLength > 128) {
+    generatePrompts("password must be no more than 128 characters.");
+    createPassword();
   }
 
-  let specialCharacters = function(){
-    confirm("Would you like to include special characters in your password?")
-    if (specialCharacters) {
-      allPossibleOptions = allPossibleOptions.concat(allPossibleSpecialChar); 
-    }
-    console.log(allPossibleOptions);
+  //confirm types of characters user would like to include
+  var askPrompts = function () {
+    generatePrompts(
+      "Would you like to include special characters in your password?",
+      "confirm",
+      allPossibleSpecialChar
+    );
+    generatePrompts(
+      "Would you like to include numbers in your password?",
+      "confirm",
+      allPossibleNumbers
+    );
+    generatePrompts(
+      "Would you like to include lowercase letetrs in your password?",
+      "confirm",
+      allPossibleLowerCase
+    );
+    generatePrompts(
+      "Would you like to include uppercase letters in your password?",
+      "confirm",
+      allPossibleUpperCase
+    );
   };
-  
-  let upperCase = function() {
-    confirm("Would you like to include uppercase letters in your password?");
-    if(upperCase) {
-      allPossibleOptions = allPossibleOptions.concat(allPossibleUpperCase);
-    }
-    console.log(allPossibleOptions);
-  };
-  
-  let lowerCase = function() {
-    confirm("Would you like to include lowercase letters in your password?");
-    if (lowerCase) {
-      allPossibleOptions = allPossibleOptions.concat(allPossibleLowerCase);
-    }
-    console.log(allPossibleOptions);
-  };
-  
-  let num = function() {
-    confirm("Would you like to include numners in your password?");
-    if (num) {
-      allPossibleOptions = allPossibleOptions.concat(allPossibleNumbers)
-    }
-    console.log(allPossibleOptions);
-  };
-  specialCharacters();
-  upperCase();
-  lowerCase();
-  num();
 
-};
-userPrompt();
+  askPrompts();
+  writePassword();
+}
 
+//function to randomly create password from the allPossibleOptions array
+function generatePassword() {
+  var password = [];
 
+  for (var i = 0; i < passwordLength; i++) {
+    var returnedChar =
+      allPossibleOptions[Math.floor(Math.random() * allPossibleOptions.length)];
+    password.push(returnedChar);
+  }
 
+  return password.join("");
+}
 
-
-// Write password to the #password input
+//function to generate my password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", createPassword);
